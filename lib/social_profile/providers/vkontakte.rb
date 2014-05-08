2242df1610040f96c07489bed8ad5f00f4158697
superp <superp1987@gmail.com>
2014-05-08 13:27:49 +0300

    parse new vkontakte response

--- lib/social_profile/providers/vkontakte.rb
+++ lib/social_profile/providers/vkontakte.rb
@@ -1,11 +1,10 @@
-# encoding: utf-8
 module SocialProfile
   module Providers
     class Vkontakte < Base      
       def picture_url
         @picture_url ||= begin
-          if auth_hash['extra'] && auth_hash['extra']['raw_info']
-            photo = auth_hash['extra']['raw_info']['photo_big']
+          if auth_hash['extra'] && info = auth_hash['extra']['raw_info']
+            photo = info['photo_big'] || info['photo_200_orig']
             Utils.blank?(photo) ? nil : photo
           end
         end
