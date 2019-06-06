ff1507904254cb1876eafa77d8c06e29af68c963
o.vykhor <o.vykhor@ukr.net>
2019-06-06 19:20:18 +0300

    Updated Google Person

--- lib/social_profile/version.rb
+++ lib/social_profile/version.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 module SocialProfile
-  VERSION = "0.3.5"
+  VERSION = "0.3.6"
 end
--- spec/people/google_spec.rb
+++ spec/people/google_spec.rb
@@ -1,24 +1,57 @@
 require 'spec_helper'
 
 describe SocialProfile::People::Google do
+  let(:user) { SocialProfile::Person.get(:google, nil, ENV['GOOGLE_ACCESS_TOKEN']) }
+
   it 'should be a Module' do
     expect(described_class).to be_a Module
   end
 
-  context 'google' do
-    let(:user) { SocialProfile::Person.get(:google, '123456', 'abc') }
+  it 'should be a google profile' do
+    expect(user).to be_a described_class
+  end
 
-    before do
-      stub_request(:get, 'https://www.googleapis.com/youtube/v3/channels?mine=true&part=statistics').
-        to_return(status: 200, body: fixture('google/channels.json'))
-    end
+  it 'should response to friends_count' do
+    expect(user.friends_count).to be_a Integer
+  end
 
-    it 'should be a google profile' do
-      expect(user).to be_a described_class
-    end
+  it 'fetches channel title' do
+    expect(user.channel_title).to be_a String
+  end
 
-    it 'should response to friends_count' do
-      expect(user.friends_count).to eq 2
-    end
+  it 'fetches channel keywords' do
+    expect(user.channel_keywords).to be_an Array
+  end
+
+  it 'fetches channel video count' do
+    expect(user.channel_video_count).to be_an Integer
+  end
+
+  it 'fetches channel videos view count' do
+    expect(user.channel_view_count).to be_an Integer
+  end
+
+  it 'fetches channel country' do
+    expect(user.channel_country).to be_a String
+  end
+
+  it 'fetches channel categories' do
+    expect(user.channel_categories).to be_any
+  end
+
+  it 'fetches subscription channels' do
+    expect(user.subscription_channels).to all(be_a(described_class))
+  end
+
+  it 'fetches last videos like count' do
+    expect(user.last_like_count).to be_an Integer
+  end
+
+  it 'fetches last videos view count' do
+    expect(user.last_view_count).to be_an Integer
+  end
+
+  it 'fetches last videos comment count' do
+    expect(user.last_comment_count).to be_an Integer
   end
 end
